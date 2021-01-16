f56d575384ecde7dc7c2ffd62c5b79d6e8b94ec5
SUMIT_THAKUR <sumit.thakur@sellerapp.com>
2021-01-16 17:47:29 +0530

    fix changes

--- api/admin/admin.go
+++ api/admin/admin.go
@@ -15,21 +15,21 @@ func RegisterAdminService(srv *grpc.Server) {
 }
 
 func (*Server) EditUserDetails(ctx context.Context, req *adminpb.EditUserDetailsRequest) (*adminpb.CommonResponse, error) {
-	
+	return nil, nil
 }
 
 func (*Server) AdminCreateTweet(ctx context.Context, req *adminpb.TweetAdminRequest) (*adminpb.CommonResponse, error) {
-
+	return nil, nil
 }
 
 func (*Server) AdminDeleteTweet(ctx context.Context, req *adminpb.DeleteAdminTweetRequest) (*adminpb.CommonResponse, error) {
-
+	return nil, nil
 }
 
 func (*Server) AdminUpdateTweet(ctx context.Context, req *adminpb.AdminTweetUpdateRequest) (*adminpb.CommonResponse, error) {
-
+	return nil, nil
 }
 
 func (*Server) AdminListTweet(ctx context.Context, req *adminpb.AdminTweetListRequest) (*adminpb.TweetListResponse, error) {
-
+	return nil, nil
 }
